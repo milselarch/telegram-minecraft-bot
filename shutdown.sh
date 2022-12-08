731aa9834041c7cac27e844c68ff9eb32ddc7a20
milselarch <milselarch@gmail.com>
2022-12-08 11:58:09 +0000

    use absolute paths in shell scripts

--- shutdown.sh
+++ shutdown.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo tmux send -t minecraft.0 " " ENTER
 sudo tmux send -t minecraft.0 "/save-all" ENTER
-sudo tmux send -t minecraft.0 "/save-off" ENTER
+:sudo tmux send -t minecraft.0 "/save-off" ENTER
 sudo tmux send -t minecraft.0 " " ENTER
 sudo tmux send -t minecraft.0 "/stop" ENTER
 /usr/bin/gsutil cp -R /home/minecraft/world gs://minecraft_gcp_telegram_backups/$(date "+%Y%m%d-%H%M%S")-world
